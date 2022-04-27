5dd53fe3ca09a1d6622cdbb1cccd234f0c4d52b3
Murugan2207 <vickyvenkat2207@gmail.com>
2022-04-27 17:26:23 +0530

    first commit

---
+++ src/main/java/com/movieticketapp/movieticketappapi/MovieticketappApiApplication.java
@@ -0,0 +1,13 @@
+package com.movieticketapp.movieticketappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieticketappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieticketappApiApplication.class, args);
+	}
+
+}
